400ca8329bff0fdc29556c1b7b90760d91edd214
Oleby0406 <oleg.bychenkov@gmail.com>
2021-02-03 22:48:57 -0800

    Graphs use vbox instead of whatever they used before

--- app/src/main/java/covidproject/GraphTest.java
+++ app/src/main/java/covidproject/GraphTest.java
@@ -11,12 +11,20 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis; 
 import javafx.scene.chart.XYChart;
 import javafx.util.StringConverter;
+import javafx.scene.layout.VBox;
 
 public class GraphTest {
     LineChart linechart;
-    XYChart.Series series = new XYChart.Series(); 
+    VBox box = new VBox();
+    XYChart.Series series = new XYChart.Series();
+    public GraphTest() { 
+    }
 
-    public GraphTest(ArrayList xValues, ArrayList yValues) {
+    public VBox getGraph() {
+        return box;
+    }
+
+    public void updateGraph(DataVaccinations data) {
         NumberAxis xAxis = new NumberAxis(1609488000000L, new Date().getTime(), 86400000); 
         xAxis.setLabel("Time");   
         xAxis.setTickLabelFormatter((StringConverter<Number>)(new DateCoordinate()));
@@ -28,18 +36,17 @@ public class GraphTest {
         linechart.setAnimated(false);
 
         series.setName("Vaccines over time");
-    }
-    public LineChart getGraph() {   
-        return linechart;
-    }
 
-    public void updateGraph(ArrayList xValues, ArrayList yValues) {
         series.getData().clear();
-        for (int i = 0; i < xValues.size(); i++) {
-            series.getData().add(new XYChart.Data(xValues.get(i), yValues.get(i)));
+
+        for (int i = 0; i < data.getXValues().size(); i++) {
+            series.getData().add(new XYChart.Data(data.getXValues().get(i), data.getYValues().get(i)));
         }
+
         linechart.getData().clear();
         linechart.getData().add(series);
+
+        box.getChildren().clear();
+        box.getChildren().add(linechart);
     }
 }
-
