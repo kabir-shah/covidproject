ee2cfc1e5c459ab390f1e6c633694e098a33fa5b
Oleby0406 <oleg.bychenkov@gmail.com>
2021-02-04 00:35:21 -0800

    was cursed but now kinda epic

--- app/src/main/java/covidproject/GraphTest.java
+++ app/src/main/java/covidproject/GraphTest.java
@@ -22,32 +22,42 @@ public class GraphTest {
         return box;
     }
 
-    public void updateGraph(DataVaccinations data) {
-        LineChart linechart;
-        XYChart.Series series = new XYChart.Series();
-        NumberAxis xAxis = new NumberAxis(1609488000000L, new Date().getTime(), 86400000); 
-        xAxis.setLabel("Time");   
-        xAxis.setTickLabelFormatter((StringConverter<Number>)(new DateCoordinate()));
-        long maxValue = data.getYValues().get(data.getYValues().size() - 1);
-
-        NumberAxis yAxis = new NumberAxis(0, maxValue * 11 / 10, maxValue / 10); 
-        yAxis.setLabel("Vaccines");
-
-        linechart = new LineChart(xAxis, yAxis);
-        linechart.setAnimated(false);
-
-        series.setName("Vaccines over time");
-
-        series.getData().clear();
-
-        for (int i = 0; i < data.getXValues().size(); i++) {
-            series.getData().add(new XYChart.Data(data.getXValues().get(i), data.getYValues().get(i)));
-        }
-
-        linechart.getData().clear();
-        linechart.getData().add(series);
-
+    public void updateGraph(ArrayList<DataVaccinations> data) {
         box.getChildren().clear();
-        box.getChildren().add(linechart);
+        long maxValue = data.get(0).getYValues().get(data.get(0).getYValues().size() - 1);
+        for (int k = 1; k < data.size(); k++) {
+            if (data.get(k).getYValues().get(data.get(k).getYValues().size() - 1) > maxValue) {
+                maxValue = data.get(k).getYValues().get(data.get(k).getYValues().size() - 1);
+            }
+        }
+        for (int j = 0; j < data.size(); j++) {
+            LineChart linechart;
+            XYChart.Series series = new XYChart.Series();
+            NumberAxis xAxis = new NumberAxis(1609488000000L, new Date().getTime(), 86400000); 
+            xAxis.setLabel("Time");   
+            xAxis.setTickLabelFormatter((StringConverter<Number>)(new DateCoordinate()));
+            
+            NumberAxis yAxis = new NumberAxis(0, maxValue * 11 / 10, maxValue / 10); 
+            yAxis.setLabel("Vaccines");
+
+            linechart = new LineChart(xAxis, yAxis);
+            linechart.setAnimated(false);
+            linechart.setTitle("Vaccines dispensed in " + data.get(j).getLocation());
+
+            series.setName("Vaccines over time");
+
+
+            series.getData().clear();
+
+            for (int i = 0; i < data.get(j).getXValues().size(); i++) {
+                series.getData().add(new XYChart.Data(data.get(j).getXValues().get(i), data.get(j).getYValues().get(i)));
+            }
+
+            linechart.getData().clear();
+            linechart.getData().add(series);
+
+            
+            box.getChildren().add(linechart);
+        }
     }
 }
