cbce39773b58990b3979ffc4a9bd8cf6caa9bd7d
Kabir Shah <iamkbrsh@gmail.com>
2021-02-08 18:39:33 -0800

    add comments

--- app/src/main/java/covidproject/DataDailyVaccinations.java
+++ app/src/main/java/covidproject/DataDailyVaccinations.java
@@ -7,24 +7,45 @@ public class DataDailyVaccinations {
 	private ArrayList<Number> xs;
 	private ArrayList<Integer> ys;
 
+	/**
+	 * Create a new instance of this data structure.
+	 * @param {String} location
+	 */
 	public DataDailyVaccinations(String l) {
 		location = l;
 		xs = new ArrayList<Number>();
 		ys = new ArrayList<Integer>();
 	}
 
+	/**
+	 * Location getter.
+	 * @return {String} location
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * X Values getter.
+	 * @return {ArrayList<Number>} x values
+	 */
 	public ArrayList<Number> getXValues() {
 		return xs;
 	}
 
+	/**
+	 * Y Values getter.
+	 * @return {ArrayList<Integer>} y values
+	 */
 	public ArrayList<Integer> getYValues() {
 		return ys;
 	}
 
+	/**
+	 * Add a new entry into the data structure.
+	 * @param {Number} x value
+	 * @param {Integer} y value
+	 */
 	public void add(Number x, Integer y) {
 		xs.add(x);
 		ys.add(y);
--- app/src/main/java/covidproject/DataVaccinationsPerHundred.java
+++ app/src/main/java/covidproject/DataVaccinationsPerHundred.java
@@ -7,24 +7,45 @@ public class DataVaccinationsPerHundred {
 	private ArrayList<Number> xs;
 	private ArrayList<Float> ys;
 
+	/**
+	 * Create a new instance of this data structure.
+	 * @param {String} location
+	 */
 	public DataVaccinationsPerHundred(String l) {
 		location = l;
 		xs = new ArrayList<Number>();
 		ys = new ArrayList<Float>();
 	}
 
+	/**
+	 * Location getter.
+	 * @return {String} location
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * X Values getter.
+	 * @return {ArrayList<Number>} x values
+	 */
 	public ArrayList<Number> getXValues() {
 		return xs;
 	}
 
+	/**
+	 * Y Values getter.
+	 * @return {ArrayList<Float>} y values
+	 */
 	public ArrayList<Float> getYValues() {
 		return ys;
 	}
 
+	/**
+	 * Add a new entry into the data structure.
+	 * @param {Number} x value
+	 * @param {Integer} y value
+	 */
 	public void add(Number x, Float y) {
 		xs.add(x);
 		ys.add(y);
